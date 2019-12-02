c80ee3bc872f0ba2f831e255e12bae6829be5f6b
Ralf Hortt <me@horttcore.de>
2019-12-02 08:35:51 +0100

    v2.0.0
    - Add: Pass arguments to service register function
    - Change: Namespace changed to RalfHortt\Plugin
    - Change: Move translation to its own service
    
    Signed-off-by: Ralf Hortt <me@horttcore.de>

--- README.md
+++ README.md
@@ -2,25 +2,36 @@
 
 ## Usage
 
-* Create a plugin container
-* Add a service
-* Boot the plugin
+- Create a plugin container
+- Add a service
+- Boot the plugin
+
+## Installation
+
+`$ composer require ralfhortt/wp-plugin`
 
 ## Example
+
 ```php
 <?php
 PluginFactory::create()
-    ->addTranslation($textdomain, $pathToDirectory)
-    ->addService(Service::class)
+    ->addService(Service::class) // Simple service
+    ->addService(anotherService::class, 'Foo', ['bar']) // Service that passes args to his register method
     ->boot();
 ```
 
 ## Changelog
 
+### v2.0.0
+
+- Add: Pass arguments to service register function
+- Change: Namespace changed to RalfHortt\Plugin
+- Change: Move translation to its own service
+
 ### v1.0.1
 
-* Changed: `PluginFactory` returns a new `Plugin` object instead of a shared instance
+- Changed: `PluginFactory` returns a new `Plugin` object instead of a shared instance
 
 ### v1.0.0
 
-* Initial release
+- Initial release
--- src/Plugin.php
+++ src/Plugin.php
@@ -1,5 +1,7 @@
 <?php
-namespace Horttcore\Plugin;
+namespace RalfHortt\Plugin;
+
+use const Exception;
 
 /**
  * Class Plugin.
@@ -7,39 +9,19 @@ namespace Horttcore\Plugin;
  * Main plugin controller class that hooks the plugin's functionality into the WordPress request lifecycle.
  *
  * @since   1.0.0
- * @package Horttcore\Plugin
+ * @package RalfHortt\Plugin
  * @author  Ralf Hortt <me@horttcore.de>
  */
 class Plugin
 {
 
-
     /**
      * Services
-     * 
+     *
      * @since 1.0.0
      * @var array $services Registered services
      */
     protected $services = [];
-
-
-    /**
-     * Textdomain
-     * 
-     * @since 1.0.0
-     * @var string $textdomain Textdomain
-     */
-    protected $textdomain = '';
-
-
-    /**
-     * Path to translation folder
-     * 
-     * @since 1.0.0
-     * @var string $translationPath Path to translations
-     */
-    protected $translationPath = '';
-
 
     /**
      * Register the plugin with the WordPress system.
@@ -49,8 +31,7 @@ class Plugin
      */
     public function boot()
     {
-        add_action('plugins_loaded', [$this, 'registerServices']);
-        add_action('plugins_loaded', [$this, 'loadTranslation']);
+        \add_action('plugins_loaded', [$this, 'registerServices']);
 
         return $this;
     }
@@ -62,9 +43,15 @@ class Plugin
      * @param strong $class Class name
      * @return Plugin
      **/
-    public function addService( string $class )
+    public function addService(string $class)
     {
-        $this->services[] = $class;
+        $args = func_get_args();
+        array_shift($args);
+
+        $this->services[] = [
+            'class' => $class,
+            'args' => $args
+        ];
 
         return $this;
     }
@@ -87,45 +74,17 @@ class Plugin
      * Register the individual services of this plugin.
      *
      * @since 1.0.0
+     * @throws Exception
      */
     public function registerServices()
     {
         $services = $this->getServices();
         array_walk($services, function ($service) {
+            if (!class_exists($service['class']) || !method_exists($service['class'], 'register')) {
+                throw new Exception('Service is not registerable');
+            }
 
-            if ( !class_exists( $service ) || !method_exists( $service, 'register' ) )
-                return;
-
-                (new $service)->register();
+            (new $service['class'])->register(...$service['args']);
         });
     }
-
-
-    /**
-     * Set a textdomain
-     *
-     * @param string $textdomain Textdomain
-     * @param string $translationPath Path to translation folder
-     * @return Plugin
-     **/
-    public function addTranslation(string $textdomain, $translationPath): Plugin
-    {
-        $this->textdomain = $textdomain;
-        $this->translationPath = $translationPath;
-
-        return $this;
-    }
-
-
-    /**
-     * Load translation
-     *
-     * @return bool If translation is loaded
-     **/
-    public function loadTranslation()
-    {
-        return load_plugin_textdomain( $this->textdomain, false, $this->translationPath );
-    }
-
-
 }
