074af7f68a619be9629c047de8b4423b5338ac09
Ralf Hortt <me@horttcore.de>
2020-05-25 09:58:17 +0000

    Apply fixes from StyleCI

--- src/Plugin.php
+++ src/Plugin.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace RalfHortt\Plugin;
 
 /**
@@ -7,17 +8,17 @@ namespace RalfHortt\Plugin;
  * Main plugin controller class that hooks the plugin's functionality into the WordPress request lifecycle.
  *
  * @since   1.0.0
- * @package RalfHortt\Plugin
+ *
  * @author  Ralf Hortt <me@horttcore.de>
  */
 class Plugin
 {
-
     /**
-     * Services
+     * Services.
      *
      * @since 1.0.0
-     * @var array $services Registered services
+     *
+     * @var array Registered services
      */
     protected $services = [];
 
@@ -25,6 +26,7 @@ class Plugin
      * Register the plugin with the WordPress system.
      *
      * @since 1.0.0
+     *
      * @return Plugin
      */
     public function boot()
@@ -34,11 +36,11 @@ class Plugin
         return $this;
     }
 
-
     /**
-     * Add service
+     * Add service.
      *
      * @param strong $class Class name
+     *
      * @return Plugin
      **/
     public function addService(string $class)
@@ -48,12 +50,11 @@ class Plugin
 
         $this->services[] = [
             'class' => $class,
-            'args' => $args
+            'args'  => $args,
         ];
 
         return $this;
     }
-
 
     /**
      * Get the list of services to register.
@@ -67,11 +68,11 @@ class Plugin
         return $this->services;
     }
 
-
     /**
      * Register the individual services of this plugin.
      *
      * @since 1.0.0
+     *
      * @throws Exception
      */
     public function registerServices()
