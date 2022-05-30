41a90cc6a0cf0b4f2c83ecbce05a26d692af320b
Ralf Hortt <me@horttcore.de>
2022-05-30 11:16:38 +0200

    Apply fixes from StyleCI (#2)

--- src/Plugin.php
+++ src/Plugin.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace RalfHortt\Plugin;
 
 /**
@@ -7,17 +8,17 @@ namespace RalfHortt\Plugin;
  * Main plugin controller class that hooks the plugin's functionality into the WordPress request lifecycle.
  *
  * @since   1.0.0
- * @package RalfHortt\Plugin
+ *
  * @author  Ralf Hortt <me@horttcore.de>
  */
 class Plugin
 {
-
     /**
-     * Services
+     * Services.
      *
      * @since 1.0.0
-     * @var array $services Registered services
+     *
+     * @var array Registered services
      */
     protected $services = [];
 
@@ -25,6 +26,7 @@ class Plugin
      * Register the plugin with the WordPress system.
      *
      * @since 1.0.0
+     *
      * @return Plugin
      */
     public function boot()
@@ -34,11 +36,11 @@ class Plugin
         return $this;
     }
 
-
     /**
-     * Add service
+     * Add service.
      *
      * @param strong $class Class name
+     *
      * @return Plugin
      **/
     public function addService(string $class)
@@ -47,19 +49,19 @@ class Plugin
         array_shift($args);
 
         $this->services[] = [
-            'hook' => 'plugins_loaded',
+            'hook'  => 'plugins_loaded',
             'class' => $class,
-            'args' => $args
+            'args'  => $args,
         ];
 
         return $this;
     }
 
-
     /**
-     * Add service
+     * Add service.
      *
      * @param strong $class Class name
+     *
      * @return Plugin
      **/
     public function addServiceHook()
@@ -69,14 +71,13 @@ class Plugin
         $class = array_shift($args);
 
         $this->services[] = [
-            'hook' => $hook,
+            'hook'  => $hook,
             'class' => $class,
-            'args' => $args
+            'args'  => $args,
         ];
 
         return $this;
     }
-
 
     /**
      * Get the list of services to register.
@@ -90,23 +91,23 @@ class Plugin
         return $this->services;
     }
 
-
     /**
      * Register the individual services of this plugin.
      *
      * @since 1.0.0
+     *
      * @throws Exception
      */
     public function registerServices()
     {
         $services = $this->getServices();
-        
+
         array_walk($services, function ($service) {
-            \add_action($service['hook'], function() use ($service) {
+            \add_action($service['hook'], function () use ($service) {
                 if (!class_exists($service['class'])) {
                     throw new \Exception(sprintf('Service is not registerable: Class %s does not exist', $service['class']));
                 }
-    
+
                 if (!method_exists($service['class'], 'register')) {
                     throw new \Exception(sprintf('Service is not registerable: Method `register` does not exist in %s', $service['class']));
                 }
